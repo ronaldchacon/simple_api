5395f8628b6fdde8ef5623c06cabd2a621b32d44
Ronald Chacon <ronald.chacon87@gmail.com>
2016-01-15 14:41:38 -0800

    csrf and cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   respond_to :json
+  before_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://myfrontend.com:3002'
+    headers['Access-Control-Request-Method'] = 'GET, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = 'x-requested-with,Content-Type, Authorization'
+  end
 end
